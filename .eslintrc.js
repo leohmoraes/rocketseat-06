742271e0f7e940f290eb0ce1beca0ff0b054b607
Leo Moraes <moraes.leo@gmail.com>
2019-10-02 18:49:45 -0300

    04 - ESLint, Prettier & EditorConfig https://github.com/leohmoraes/rocketseat-06/issues/4

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+    'prettier',
+    'prettier/react', // integração do prettier com react
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint', // para enteder as ultimas versões do Ecma Script
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier', // adicionando mais um plugin
+  ],
+  rules: {
+    'react/state-in-constructor': 'off',
+    'react/static-property-placement': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js'],
+      },
+    ],
+    'import/prefer-default-export': 'off', // para garantir que import/export sem ser apenas o i/e default
+  },
+};
