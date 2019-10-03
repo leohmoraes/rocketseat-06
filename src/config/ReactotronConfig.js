01443ab24e7a5e85e0b3917a908960f2fea9d56e
Leo Moraes <moraes.leo@gmail.com>
2019-10-03 19:54:21 -0300

    06 - React Navigation Parte 1 de 2
    yarn add react-navigation react-native-gesture-handler react-native-reanimated
    react-native run-android
    ios/pod install
    react-native run-ios

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,7 +1,7 @@
 import Reactotron from 'reactotron-react-native';
 
 if (__DEV__) {
-  // const tron = Reactotron.configure({ host: '192.168.56.101' }).useReactNative().connect(); //se for emulado via USB
+  // USB iOS const tron = Reactotron.configure({ host: '192.168.56.101' }).useReactNative().connect(); //se for emulado via USB
   // se não funcionar no emulador do Android, execute adb reverse tcp:9090 tcp:9090
   const tron = Reactotron.configure().useReactNative().connect();
   console.tron = tron;
--- src/index.js
+++ src/index.js
@@ -1,37 +1,11 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {View} from 'react-native';
 
 import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  welcomeRed: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-    color: 'red',
-  },
-});
-
-
-console.tron.log("Hello World");
-
+// console.tron.log("teste");
+// console.tron.warning("teste");
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Bem Vindo ao React Native</Text>
-      <Text style={styles.welcomeRed}>Aula 5</Text>
-    </View>
-  );
+  return <View />;
 }
