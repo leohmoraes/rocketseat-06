2c9d80451468ae1a915e303d1475296b62bc0bf5
Leo Moraes <moraes.leo@gmail.com>
2019-10-06 00:48:24 -0300

    07 - Configurando StatusBar #7 https://github.com/leohmoraes/rocketseat-06/issues/7

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 // import { SafeAreaView, View } from 'react-native';
+import { StatusBar } from 'react-native';
 
 import './config/ReactotronConfig';
 
 import Routes from './routes';
 
-console.tron.log('teste');
+console.tron.log('src/index');
 // console.tron.warning('teste');
 /*
 export default function App() {
   return <Routes />;
 }
 */
-const App = () => <Routes />;
 
-export default App;
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#715971" />
+      <Routes />
+    </>
+  );
+}
+
+// const App = () => <Routes />;
+// export default App;
