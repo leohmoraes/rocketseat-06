3b02e9b241c4cb97e4a414e967a347dc96572a03
Leo Moraes <moraes.leo@gmail.com>
2019-10-07 19:30:57 -0300

    10 - Acessando API do Github #10 Parte 1 - Formulario https://github.com/leohmoraes/rocketseat-06/issues/10

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Container, Form, Input, SubmitButton } from './styles';
 
 Icon.loadFont();
 
-export default function Main() {
-  return (
-    <Container>
-      <Form>
-        <Input
-          autoCorrect={false}
-          autoCapitalize="none"
-          placeholder="Adicionar usuário"
-        />
-        <SubmitButton>
-          <Icon name="add" size={20} color="#FFF" />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+export default class Main extends Component {
+  state = {
+    newUser: '',
+    users: [],
+  };
+
+  handleAddUser = () => {
+    console.tron.log(this.state.newUser);
+  };
+
+  render() {
+    const { users, newUser } = this.state;
+
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuário"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton onPress={this.handleAddUser}>
+            <Icon name="add" size={20} color="#FFF" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
 
 Main.navigationOptions = {
