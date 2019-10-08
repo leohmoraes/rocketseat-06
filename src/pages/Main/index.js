7fb0fb9a89fa835982a74ce8a9027f7258f18b56
Leo Moraes <moraes.leo@gmail.com>
2019-10-08 12:47:34 -0300

    10 - Acessando API do Github #10 https://github.com/leohmoraes/rocketseat-06/issues/10 Parte 2 - Final - Conexao com a API do Github, usando Axios

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Keyboard } from 'react-native';
 import { Container, Form, Input, SubmitButton } from './styles';
+import api from '../../services/api';
 
 Icon.loadFont();
 
@@ -10,8 +12,20 @@ export default class Main extends Component {
     users: [],
   };
 
-  handleAddUser = () => {
-    console.tron.log(this.state.newUser);
+  handleAddUser = async () => {
+    // console.tron.log(this.state.newUser);
+    const { users, newUser } = this.state; // users para poder adicionar na lista
+    const response = await api.get(`/users/${newUser}`);
+    const data = {
+      name: response.data.name, // nom do usuario
+      login: response.data.login, // login do usuario
+      bio: response.data.bio, // biografia
+      avatar: response.data.avatar_url, // imagem
+    };
+
+    this.setState({ users: [...users, data], newUser: '' }); // atualiza o array e zera o newUser
+
+    Keyboard.dismiss();
   };
 
   render() {
