dbb9efdf175c11defa073171ddfec34269bc3b1e
Leo Moraes <moraes.leo@gmail.com>
2019-10-08 18:49:48 -0300

    12 - Loading e disabled https://github.com/leohmoraes/rocketseat-06/issues/12

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Keyboard } from 'react-native';
-import { Container, Form, Input, SubmitButton } from './styles';
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  Avatar,
+  User,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText,
+} from './styles';
+
 import api from '../../services/api';
 
 Icon.loadFont();
@@ -47,6 +60,21 @@ export default class Main extends Component {
             <Icon name="add" size={20} color="#FFF" />
           </SubmitButton>
         </Form>
+
+        <List
+          data={users}
+          renderItem={({ item }) => (
+            <User>
+              <Avatar source={{ uri: item.avatar }} />
+              <Name>{item.name}</Name>
+              <Bio>{item.bio}</Bio>
+              <ProfileButton onPress={() => {}}>
+                <ProfileButtonText>Ver perfil</ProfileButtonText>
+              </ProfileButton>
+            </User>
+          )}
+          keyExtractor={item => item.login}
+        />
       </Container>
     );
   }
