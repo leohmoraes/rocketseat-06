93ef3bd75e5eb5adc915e93cd03c483f28bd1364
Leo Moraes <moraes.leo@gmail.com>
2019-10-11 19:53:34 -0300

    Aula 15 - Buscando dados da API https://github.com/leohmoraes/rocketseat-06/issues/15

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { View } from 'react-native';
+import api from '../../services/api'; // repositorios favoritos / starred
 
 // import { Container } from './styles';
 
@@ -8,7 +10,52 @@ import { View } from 'react-native';
 //   console.tron.log(props);
 //   return <View />;
 
-export default function User({ navigation }) {
-  console.tron.log(navigation.getParam('user'));
-  return <View />;
+/*
+Transformar em classe para acessar as classes, estados, ciclo de vida.
+*/
+export default class User extends Component {
+  // titulo da pagina
+
+  /* isto abaixo nao vai funcionar, para obter as informacoes precisa transformar em uma funcao que retorna um objeto
+  static navigationOptions = {
+    title: this.props.navigation.getParam('user').name,
+  };
+  */
+
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('user').name,
+  });
+  // ({ navigation }) {
+  // console.tron.log(navigation.getParam('user'));
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+    }).isRequired,
+  };
+
+  state = {
+    stars: [],
+  };
+
+  // Executa quando entrar na tela
+  async componentDidMount() {
+    const { navigation } = this.props;
+    console.tron.log(navigation.getParam('user'));
+    const user = navigation.getParam('user');
+
+    const response = await api.get(`/users/${user.login}/starred`);
+    this.setState({ stars: response.data });
+  }
+
+  render() {
+    const { stars } = this.state;
+    return <View />;
+  }
 }
+
+// User.propTypes = {
+//   navigation: PropTypes.shape({
+//     getParam: PropTypes.func,
+//   }).isRequired,
+// };
